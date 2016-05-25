62c97a078241398b951668f42bd5e58a168f82d2
Travis Wichtendahl <traviswichtendahl@gmail.com>
2016-05-24 18:05:34 -0700

    Directory Structure for News application
    Creates a separate subdirectory "News" for the List/View news
    application, as well as a simple news list file (index.php) and a blank
    news view file. This design is *not* compatible with Sessions data, and
    is based on the structure of our Surveys app.
    
    ToDo: Add CategoryID as a foreign key to Foreign db table, make a
    primary key for Foreign, Populate Foreign with RSS data upon page load,
    add Sessions, design news_view partial

---
+++ news/news_view.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * news_view.php pairs with /news/index.php to allow visitors to view news details
+ * 
+ * @package news_aggregator
+ * @author Ian Bryan, Rattana Neak, Travis Wichtendahl <traviswichtendahl@gmail.com>
+ * @version 1.0 2016/05/12
+ * @link http://www.twichtendahl.com/
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @see 
+ * @see Pager.php 
+ * @todo 
+ */
